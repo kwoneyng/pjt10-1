97d3c87569c98aa357e6cb34a6f8e91ba1bad710
heecheol1508 <heecheol1508@gmail.com>
2019-11-20 15:51:09 +0900

    finish modeling

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+
+# Customizing User model
+class User(AbstractUser):
+    followers = models.ManyToManyField( # followers: 나를 팔로우하는 사람들
+        settings.AUTH_USER_MODEL,
+        related_name='followings'       # followings: 내가 팔로우하는 사람들
+    )
